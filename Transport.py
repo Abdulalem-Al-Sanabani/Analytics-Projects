2eea7ebb63f7be71aa8940f7f17e8d64bf29c789
Abdulalem-Al-Sanabani <167649374+Abdulalem-Al-Sanabani@users.noreply.github.com>
2024-05-31 15:12:24 +0000

    finish cleaning

--- Transport.py
+++ Transport.py
@@ -2,10 +2,14 @@ from cleaners import Cleaner
 import pandas as pd 
 
 df=pd.read_excel("26_May_2024 _Daily.xlsx", sheet_name="67")
-print(df.info())
+# print(df.info())
 
 cln=Cleaner()
 
 df_1=cln.clean_df(df)
+
+df_1[df_1.columns[df_1.columns.str.contains("Date")]].info()
+
 df_2=cln.prep(df_1)
-print(df_2.columns[-3:-1])
+print(df_2.columns[-3:])
+
